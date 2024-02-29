d792adbb20d24887e6bc5395eadf7e3228f50559
alesandrani <alesandrani@outlook.es>
2024-02-29 13:24:52 +0100

    v2

--- Actividad18Muniemon/src/actividad18/MainMuniemon.java
+++ Actividad18Muniemon/src/actividad18/MainMuniemon.java
@@ -9,21 +9,23 @@ public class MainMuniemon {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		menu();
-		
-		
 	}
 	public static void menu() {
 		Scanner sc = new Scanner(System.in);
 		//m = null;
 		Muniemon muniemon = null;
+		Muniemon muniemon2 = null;
 		int opcion;
 		do {
 			System.out.println("-------Menu:)---------");
 			System.out.println("1.Dar de alta Muniemon");
-			System.out.println("2.Mostar Muniemon");
-			System.out.println("3.Salir del programa");
+			System.out.println("2.Dar de alta segundo muniemon");
+			System.out.println("3.Mostrar primer muniemon");
+			System.out.println("4.Mostrar segundo muniemon");
+			System.out.println("5. Atacar primer Muniemon");
+			System.out.println("6. Atacar segundo muniemon");
+			System.out.println("7.Salir del programa");
 			System.out.println("-----Seleccione una opcion!-----");
 			opcion = sc.nextInt();
 			switch(opcion) {
@@ -31,16 +33,30 @@ public class MainMuniemon {
 				muniemon = altaMuniemon(sc);
 				break;
 			case 2:	
+				muniemon2 = altaMuniemon(sc);
+				break;
+			case 3:	
 				mostrarMuniemon(muniemon);
 				break;
-			case 3:
+			case 4:	
+				mostrarMuniemon(muniemon2);
+				break;
+			case 5:
+				//atacarMuniemon(muniemon, muniemon2);
+				muniemon.atacar(muniemon2);
+				break;
+			case 6:
+				//atacarMuniemon(muniemon2, muniemon);
+				muniemon2.atacar(muniemon);
+				break;
+			case 7:
 				System.out.println("Salir del programa..");
 				break;
 				default:
 					System.out.println("Opcion no es valida. Por favor, seleccione una opcion valida");
 			}
 		
-	} while (opcion != 3);
+	} while (opcion != 7);
 		
 	}
 	public static Muniemon altaMuniemon(Scanner sc) {
@@ -59,7 +75,7 @@ public class MainMuniemon {
 		}
 		int seleccion =sc.nextInt();
 		TipoMuniemon tm = TipoMuniemon.values()[seleccion];
-		System.out.println("usted ha seleccionado : " + tm);
+		System.out.println("Selecionaste : " + tm);
 		Muniemon muniemon = new Muniemon (nombre, vida, ataque, defensa, tm);
 		System.out.println("MUNIEMON CREADO!OLE!");
 		return muniemon;
@@ -76,5 +92,9 @@ public class MainMuniemon {
 		}else {
 			System.out.println("No hay muniemon  creado aun");
 		}
+	}
+	
+	public static void atacarMuniemon(Muniemon atacante, Muniemon atacado) {
+		atacado.atacar(atacante);
 	}
 }
--- Actividad18Muniemon/src/actividad18/Muniemon.java
+++ Actividad18Muniemon/src/actividad18/Muniemon.java
@@ -6,11 +6,11 @@ public class Muniemon {
 	private int ataque;
 	private int defensa;
 	private TipoMuniemon tipo;
-	
+
 	public Muniemon() {
 		super();
 	}
-	
+
 	public Muniemon(String nombre, int vida, int ataque, int defensa, TipoMuniemon muniemon) {
 		super();
 		this.nombre = nombre;
@@ -19,43 +19,76 @@ public class Muniemon {
 		this.defensa = defensa;
 		this.tipo = muniemon;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Muniemon [nombre=" + nombre + ", vida=" + vida + ", ataque=" + ataque + ", defensa=" + defensa
 				+ ", muniemon=" + tipo + "]";
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public int getVida() {
 		return vida;
 	}
+
 	public void setVida(int vida) {
 		this.vida = vida;
 	}
+
 	public int getAtaque() {
 		return ataque;
 	}
+
 	public void setAtaque(int ataque) {
 		this.ataque = ataque;
 	}
+
 	public int getDefensa() {
 		return defensa;
 	}
+
 	public void setDefensa(int defensa) {
 		this.defensa = defensa;
 	}
+
 	public TipoMuniemon getMuniemon() {
 		return tipo;
 	}
+
 	public void setMuniemon(TipoMuniemon muniemon) {
 		this.tipo = muniemon;
 	}
-	
-	
+
+	public void atacar(Muniemon objetivo) {
+		System.out.println(this.nombre + "ataca a " + objetivo.getNombre() + "!");
+		if (this.vida < 0) {
+			System.out.println("No se puede atacar porque " + this.nombre + "esta muerto");
+
+		} else {
+			if (objetivo.getVida() <= 0) {
+				System.out.println(objetivo.getNombre() + "ya esta muerto. No se puede atacar");
+
+			}else {
+				int ataqueReal = this.ataque - objetivo.defensa;
+				System.out.println("El ataque real es: " + ataqueReal);
+				if(ataqueReal > 0) {
+					objetivo.vida -= ataqueReal;
+					System.out.println(objetivo.getNombre() + " pierde " + ataqueReal + " puntos de vida.");
+					if (objetivo.getVida() <= 0) {
+						System.out.println(objetivo.getNombre() + " ha sido derrotado.");
+					}
+				}
+				
+			}
+			
+		}
+
+	}
 }
